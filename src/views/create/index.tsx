e90431c520305360e599f934bb043253ad225c0d
VORDIUM <Vordium@gmail.com>
2024-03-01 00:58:21 +0400

    create

--- src/views/create/index.tsx
+++ src/views/create/index.tsx
@@ -6,7 +6,7 @@ export const CreateView: FC = ({}) => {
     <div className="mx-auto p-4 md:hero">
       <div className="flex flex-col md:hero-content">
         <h1 className="bg-gradient-to-tr from-[#9945FF] to-[#14F195] bg-clip-text text-center text-5xl font-bold text-transparent">
-          Create solana fungible token
+          Create SPL Token
         </h1>
         {/* CONTENT GOES HERE */}
         <div className="text-center">
